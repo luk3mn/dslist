46952335a7088f8d29d669dd5e02b967ffcc41a9
luk3mn <lucasrenan2015@gmail.com>
2023-11-16 10:50:33 -0300

    feat: homolog ok and ready to deploy

--- dslist/src/main/java/com/luke/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/luke/dslist/repositories/GameRepository.java
@@ -12,7 +12,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     // Now we are able to use a component query, insert, update and delete whatever we need from sql, but using java 
 
     @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 		tb_game.short_description AS shortDescription, tb_belonging.position
 		FROM tb_game
 		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
